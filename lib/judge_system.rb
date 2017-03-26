c8823b00a38152df0c2605493090a90fa5f20346
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-03-26 14:11:52 +0900

    [update] create python3 system

--- lib/judge_system.rb
+++ lib/judge_system.rb
@@ -31,7 +31,7 @@ module JudgeSystem
 			spliter = "\n<$><*><$>\n"
 			stdin = code + spliter + input + spliter +  ("%f" % time)
 			begin
-			p	data = compile( compiler: "gcc-head", code: sys, stdin: stdin )
+				data = compile( compiler: "gcc-head", code: sys, stdin: stdin )
 			rescue
 				return 'RE'
 			end
